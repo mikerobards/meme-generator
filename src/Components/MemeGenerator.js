cfac4146dab85c35fb4d471ac3aecf6db2440cc2
Michael Robards <mikerobards@icloud.com>
2021-07-27 17:04:17 -0400

    add img generation

--- src/Components/MemeGenerator.js
+++ src/Components/MemeGenerator.js
@@ -54,6 +54,12 @@ class MemeGenerator extends Component {
 
                     <button>Gen</button>
                 </form>
+                <div className="meme">
+                    <img src={this.state.randomImg} alt="" />
+                    <h2 className="top">{this.state.topText}</h2>
+                    <h2 className="bottom">{this.state.bottomText}</h2>
+
+                </div>
             </div>
         )
     }
